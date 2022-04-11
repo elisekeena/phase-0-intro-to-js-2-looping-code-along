3f3e136d5ece51b4228298126f330de9ce923741
Elise <elise041294@gmail.com>
2022-04-11 16:34:39 -0600

    commit tree 15e70babc512e42c16ae3c6b6b27ecc637e45371

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let thankYouMessage = [];
+    for (let i =0; i < names.length; i++) {
+        thankYouMessage.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`) 
+    }
+        return thankYouMessage
+};
+
+function countDown(number){
+        while (number >= 0) {
+            console.log(number);
+            number--;
+        }
+}
